3cbbc3cd01e3a1113f8fb045e0c64d0421d4b90f
DevisCoelhoTenorio <devis.ufal@gmail.com>
2022-07-15 14:58:46 -0300

    10 ok v2

--- src/pages/Foods.jsx
+++ src/pages/Foods.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from 'react';
+import React, { useContext } from 'react';
 import appContext from '../context/appContext';
 import Header from '../components/Header';
 import CardFoods from '../components/CardFoods';
