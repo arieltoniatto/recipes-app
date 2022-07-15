b1fd5eb84ac85ef30445393a5ac9b89639e93587
DevisCoelhoTenorio <devis.ufal@gmail.com>
2022-07-15 14:58:17 -0300

    10 ok

--- src/context/AppProvider.jsx
+++ src/context/AppProvider.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import appContext from './appContext';
+import fetchByAllFoods from '../services/fetchMeals';
 
 const INITIAL_STATE_USER = {
   email: '',
@@ -9,11 +10,25 @@ const INITIAL_STATE_USER = {
 
 function AppProvider({ children }) {
   const [user, setUser] = useState(INITIAL_STATE_USER);
+  const [cardsList, setCardList] = useState([]);
+
+  useEffect(() => {
+    const requestFoods = async () => {
+      const URL = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
+      const QTD_MAX = 12;
+      const itensList = await fetchByAllFoods(URL)
+        .then((responsejson) => responsejson.meals.slice(0, QTD_MAX));
+      setCardList(itensList);
+    };
+    requestFoods();
+  }, []);
+
   const appData = {
     user: {
       get: user,
       set: setUser,
     },
+    cardsList,
   };
 
   return (
--- src/pages/Foods.jsx
+++ src/pages/Foods.jsx
@@ -1,21 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useContext } from 'react';
+import appContext from '../context/appContext';
 import Header from '../components/Header';
-import * as API from '../services/fetchMeals';
 import CardFoods from '../components/CardFoods';
 import './Foods.css';
 
 function Foods() {
-  const [foodsCards, setFoodsCards] = useState([]);
-
-  useEffect(() => {
-    const foods = async () => {
-      const MAX_FOOD_QTD = 12;
-      const comidas = await API.fetchByAllFoods()
-        .then((response) => response.meals.slice(0, MAX_FOOD_QTD));
-      setFoodsCards(comidas);
-    };
-    foods();
-  }, []);
+  const { cardsList } = useContext(appContext);
 
   return (
     <>
@@ -26,7 +16,7 @@ function Foods() {
       />
 
       <div className="foods">
-        {foodsCards.map((food) => (
+        {cardsList.map((food) => (
           <CardFoods
             key={ food.idMeal }
             props={ { ...food } }
